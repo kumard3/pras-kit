724aa324cfff0c9b65ea99844873a72ff3662e30
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-16 19:46:18 +0530

    font added

---
+++ components/Drawer.jsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+
+export const DrawerNav = ({ children, toggleSidebar, setToggleSidebar }) => {
+  const ref = useRef();
+  useOnClickOutside(ref, () => setToggleSidebar(false));
+  return (
+    <>
+      <div
+        ref={ref}
+        className={`fixed  top-0 px-2  z-30 inset-y-0  w-1/2  transition duration-300 transform  ${
+          toggleSidebar
+            ? "translate-x-[50vw] ease-out h-full bg-black text-white"
+            : "translate-x-[100vw] ease-in  "
+        }`}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+import { useEffect } from "react";
+
+// https://usehooks.com/useOnClickOutside/
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useOnClickOutside;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
     <div className="">
-     
+      <Nav />
     </div>
-  )
+  );
 }
