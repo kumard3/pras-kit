27c1c07fa94d04080f9e31417f48944dcea992f7
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-19 13:58:28 +0530

    sally added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import BlobOne from "../components/Blobs/BlobOne";
 import BlobThree from "../components/Blobs/BlobThree";
 import BlobTwo from "../components/Blobs/BlobTwo";
 import Nav from "../components/Nav";
+import Sally from "../components/Sally";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <BlobOne />
       <BlobTwo />
       <BlobThree />
+      <Sally />
     </div>
   );
 }
