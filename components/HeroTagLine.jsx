d40da8e24e7f64ffe6f8b97bd99029998de81d4c
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-20 23:55:42 +0530

    selection done

--- components/HeroTagLine.jsx
+++ components/HeroTagLine.jsx
@@ -1,10 +1,12 @@
 const HeroTagLine = () => {
   return (
-    <div className="h-full max-w-xl">
-      <h2 className="text-6xl text-white font-semibold font-spartan ">
+    <div className="h-full max-w-xl mt-10 lg:mt-0">
+      <h2 className="text-6xl text-white font-semibold font-spartan selectionBackground">
         The React{" "}
-        <span className="gradientText font-orbitron font-bold ">Developer Kit</span> you
-        always wanted
+        <span className="gradientText font-orbitron font-bold selection:bg-transparent">
+          Developer Kit
+        </span>{" "}
+        you always wanted
       </h2>
       <p className="text-gray-300 text-md font-spartan py-4">
         Pras-Kit is a collection of React Components, Templates, and more.
