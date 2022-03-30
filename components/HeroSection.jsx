e13535f136dd69ab3d0213ebe9f06a86a6cd00ca
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-30 19:59:02 +0530

    buttons added

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,11 +1,21 @@
+import GradientButtonOne from "./Buttons/GradientButtonOne";
+import GradientButtonTwo from "./Buttons/GradientButtonTwo";
+import ExploreButton from "./Buttons/ExploreButton";
 import HeroTagLine from "./HeroTagLine";
 import Sally from "./Sally";
 
 const HeroSection = () => {
   return (
     <div className="z-40 flex justify-center items-center w-screen">
-      <div className="lg:w-1/2 lg:pl-10 py-10 lg:mt-40 flex justify-center items-start h-full w-2/3">
+      <div className="lg:w-1/2 lg:pl-10 py-10 lg:mt-40 flex flex-col justify-center items-start h-full w-2/3">
         <HeroTagLine />
+        <div className="flex w-full space-x-4 lg:hidden">
+          <GradientButtonOne />
+          <GradientButtonTwo />
+        </div>
+        <div className="flex hidden lg:block">
+          <ExploreButton/>
+        </div>
       </div>
       <div className="lg:w-1/2 hidden lg:block">
         <Sally />
